eb325a03b2f36b590bef967fa200b0f93fd5f8b1
BUB'S CANVAS <67292285+LetsGetStartedWithBub@users.noreply.github.com>
2020-08-11 00:23:08 +0530

    Update requests.js

--- requests.js
+++ requests.js
@@ -1,5 +1,5 @@
-const API_key = "65fbddc192bffc51142a792a73dd20f5"
-//const API_key = "19f84e11932abbc79e6d83f82d6d1045"
+const API_key = "Add your API KEY"
+
 const requests = {
     fetchNetflixOriginals:`/discover/tv?api_key=${API_key}&with_networks=213`,
     fetchTrending:`/trending/all/week?api_key=${API_key}&language=en-US`,
